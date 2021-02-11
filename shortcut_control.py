a22bea949771fd81a122f14a66f116537ddf7550
Jeff <jeff@Jeffs-MacBook-Pro.local>
2021-02-10 17:13:23 -0700

    shortcut compatibility fix

--- shortcut_control.py
+++ shortcut_control.py
@@ -7,31 +7,30 @@ system = platform.system()
 
 k = PyKeyboard()
 
-# Mac OS shortcuts
-shortcuts_MAC = {
-	'toggle_video': ['shift', 'command', 'v'],
-	'toggle_audio': ['shift', 'command', 'a'],
-	'toggle_chat': ['shift', 'command', 'h'],
-	'start_meeting': ['control', 'command', 'v'],
-	'toggle_minimal': ['shift', 'command', 'm'],
-	'toggle_hand_raise': ['control', 'y'],
-	'focus': [],
-	'fullscreen': ['shift', 'command', 'f']
-}
-
-# Windows shortcuts
-shortcuts_WIN = {
-	'toggle_video': [k.alt_key, 'v'],
-	'toggle_audio': [k.alt_key, 'a'],
-	'toggle_chat': [k.alt_key, 'h'],
-	'start_meeting': [],
-	'toggle_minimal': [k.alt_key, 'm'],
-	'toggle_hand_raise': [k.alt_key, 'y'],
-	'focus': [k.control_key, k.alt_key, k.shift_key],
-	'fullscreen': [k.alt_key, 'f']
-}
-
-shortcuts = shortcuts_MAC if system == 'Darwin' else shortcuts_WIN
+if system == 'Darwin':
+	# Mac OS shortcuts
+	shortcuts = {
+		'toggle_video': ['shift', 'command', 'v'],
+		'toggle_audio': ['shift', 'command', 'a'],
+		'toggle_chat': ['shift', 'command', 'h'],
+		'start_meeting': ['control', 'command', 'v'],
+		'toggle_minimal': ['shift', 'command', 'm'],
+		'toggle_hand_raise': ['control', 'y'],
+		'focus': [],
+		'fullscreen': ['shift', 'command', 'f']
+	}
+else:
+	# Windows shortcuts
+	shortcuts = {
+		'toggle_video': [k.alt_key, 'v'],
+		'toggle_audio': [k.alt_key, 'a'],
+		'toggle_chat': [k.alt_key, 'h'],
+		'start_meeting': [],
+		'toggle_minimal': [k.alt_key, 'm'],
+		'toggle_hand_raise': [k.alt_key, 'y'],
+		'focus': [k.control_key, k.alt_key, k.shift_key],
+		'fullscreen': [k.alt_key, 'f']
+	}
 
 # function to call a shortcut and optionally type out a string
 def shortcut(identifier, content = ''):
