d24354ce66de0dcdc2b50c20f2e7d645cc4b6819
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-20.local>
2021-02-17 15:27:45 -0600

    keyword added, but haven't ran it yet

--- basic.py
+++ basic.py
@@ -2,6 +2,7 @@ import speech_recognition as sr # ref: https://github.com/Uberi/speech_recogniti
 from shortcut_control import shortcut
 
 keywords = {
+	'key_phrase': 'hey listen to me',
 	'toggle_audio': 'audio',
 	'toggle_video': 'video',
 	'toggle_chat': 'chat',
@@ -17,7 +18,7 @@ while True:
 	with sr.Microphone() as source:
 		r.adjust_for_ambient_noise(source)
 		audio = r.listen(source)
-		
+
 	text = ''
 	try:
 		text = r.recognize_google(audio);
@@ -25,18 +26,32 @@ while True:
 	except:
 		print('NO SPEECH DETECTED')
 
-	if keywords['toggle_audio'] in text:
-		shortcut('toggle_audio')
-	if keywords['toggle_video'] in text:
-		shortcut('toggle_video')
-	if keywords['start_meeting'] in text:
-		shortcut('start_meeting')
-	if keywords['toggle_minimal'] in text:
-		shortcut('toggle_minimal')
-	if keywords['toggle_hand_raise'] in text:
-		shortcut('toggle_hand_raise')
-	if keywords['send_chat'] in text:
-		shortcut('toggle_chat', content = text.split(keywords['send_chat'])[1][1:] + '\n')
-		shortcut('toggle_chat')
-	if keywords['quit'] in text:
-		exit()
+	if keywords['key_phrase'] in text:
+
+		with sr.Microphone() as source:
+			r.adjust_for_ambient_noise(source)
+			audio = r.listen(source)
+
+		text = ''
+		try:
+			text = r.recognize_google(audio);
+			print(text);
+		except:
+			print('SORRY I DID NOT CATCH THAT')
+
+
+		if keywords['toggle_audio'] in text:
+			shortcut('toggle_audio')
+		if keywords['toggle_video'] in text:
+			shortcut('toggle_video')
+		if keywords['start_meeting'] in text:
+			shortcut('start_meeting')
+		if keywords['toggle_minimal'] in text:
+			shortcut('toggle_minimal')
+		if keywords['toggle_hand_raise'] in text:
+			shortcut('toggle_hand_raise')
+		if keywords['send_chat'] in text:
+			shortcut('toggle_chat', content = text.split(keywords['send_chat'])[1][1:] + '\n')
+			shortcut('toggle_chat')
+		if keywords['quit'] in text:
+			exit()
