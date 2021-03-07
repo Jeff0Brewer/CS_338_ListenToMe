cd067875dbf79181a4cebcb5e65effb5102f8922
Kathryne Tao <kathryne@Kathrynes-MacBook-Pro-2.local>
2021-03-07 04:34:46 -0600

    windowstate for mac, but not called in main code yet

--- mac_state.py
+++ mac_state.py
@@ -5,27 +5,29 @@ import os, time, threading
 # from shortcut_control import curr_system
 # if curr_system == Darwin:
 
-counter = 0
-for apps in NSWorkspace.sharedWorkspace().launchedApplications():
-	# print([apps["NSApplicationName"]])
-	counter = counter + 1
+def mac_window_control():
+	counter = 0
+	for apps in NSWorkspace.sharedWorkspace().launchedApplications():
+		# print([apps["NSApplicationName"]])
+		counter = counter + 1
 
-precision = .8
-#might need to change out png depending on dark mode or not
-img_path = 'img'
+	precision = .8
+	#might need to change out png depending on dark mode or not
+	img_path = 'img'
 
-check_image = os.path.join(img_path, 'zoomus.png')
-pos = imagesearch(check_image, precision)
+	check_image = os.path.join(img_path, 'zoomus.png')
+	pos = imagesearch(check_image, precision)
 
-if pos[0] != -1:
-    print("zoom open")
+	if pos[0] != -1:
+	    print("zoom open")
 
-else:
-	for i in range(1, counter):
-		keyDown('command')
-		press('Tab', presses=i)
-		keyUp('command')
-		pos = imagesearch(check_image, precision)
-		if pos[0] != -1:
-			print("now on zoom window")
-			break
+	else:
+		for i in range(1, counter):
+			keyDown('command')
+			press('Tab', presses=i)
+			keyUp('command')
+			pos = imagesearch(check_image, precision)
+			if pos[0] != -1:
+				print("now on zoom window")
+				break
+# mac_window_control()
